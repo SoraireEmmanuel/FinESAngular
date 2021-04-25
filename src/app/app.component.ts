558e1f90ca363f918463ebf8898e8dcfa2e7ea65
Leonel Figueira <leolp.16@gmail.com>
2021-04-25 11:58:42 -0300

    InicioProyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FinesPlusPlus';
+  finesImg = 'https://www.todoprovincial.com/wp-content/uploads/2018/09/fines.png'
 }
