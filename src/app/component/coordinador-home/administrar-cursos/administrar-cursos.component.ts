0cd203986d6de194c20c80b517cbcaaeee2220b8
Gustavo C <gcargnelutti@alu.frlp.utn.edu.ar>
2021-10-15 13:48:57 -0300

    Limpiar lista de cursos al cambiar de sede y CENS

--- src/app/component/coordinador-home/administrar-cursos/administrar-cursos.component.ts
+++ src/app/component/coordinador-home/administrar-cursos/administrar-cursos.component.ts
@@ -30,6 +30,7 @@ export class AdministrarCursosComponent implements OnInit {
   }
 
   onCensChange(id_cens: any) {
+    this.limpiarCursos();
     this.http.get(`${this.API_URL}/VerSedebyIdCens/${id_cens}`).subscribe(
       data => this.sedes = data as Array<any>
     );
@@ -39,5 +40,9 @@ export class AdministrarCursosComponent implements OnInit {
     this.http.get(`${this.API_URL}/CursosByIdSedeActivos/${this.selectedSede}`).subscribe(
       data => this.cursos = data as Array<any>
     );
+  }
+
+  limpiarCursos() {
+    this.cursos = [];
   }
 }
